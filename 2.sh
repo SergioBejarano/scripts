f4439073e2a3b25e99e81df33f26eddd31230939
SergioBejarano <157920809+SergioBejarano@users.noreply.github.com>
2025-02-05 14:45:08 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -17,13 +17,13 @@ while true; do
             read -p "Enter filename or part of it: " filename
             results=$(find "$dir" -type f -name "*$filename*" 2>/dev/null) # Buscar archivos en el directorio especificado
             echo "$results"
-            echo "Total occurrences: $(echo "$results" | wc -l)"  # Contar el número de archivos encontrados
+            echo "Total occurrences: $(echo "$results" | wc -l)"  # Contar el numero de archivos encontrados
             ;;
         2)
             read -p "Enter file path: " file
             read -p "Enter word to search: " word
-            grep -n "$word" "$file" # Buscar la palabra en el archivo e imprimir con número de línea
-            echo "Total occurrences: $(grep -c "$word" "$file")" # Contar el número de apariciones de la palabra
+            grep -n "$word" "$file" # Buscar la palabra en el archivo e imprimir con numero de linea
+            echo "Total occurrences: $(grep -c "$word" "$file")" # Contar el numero de apariciones de la palabra
             ;;
         3)
             read -p "Enter directory: " dir
@@ -32,7 +32,7 @@ while true; do
             # Buscar archivos que coincidan con el nombre y luego buscar la palabra dentro de cada uno
             find "$dir" -type f -name "*$filename*" 2>/dev/null | while read -r file; do
                 echo "Searching in: $file" # Mostrar el archivo en el que se busca la palabra
-                grep -n "$word" "$file" # Buscar la palabra dentro del archivo con número de línea
+                grep -n "$word" "$file" # Buscar la palabra dentro del archivo con numero de linea
                 echo "Total occurrences: $(grep -c "$word" "$file")" # Contar el número de apariciones de la palabra
             done
             ;;
@@ -42,7 +42,7 @@ while true; do
 
             # Verificar si el archivo existe
             if [ -f "$file" ]; then
-                echo "Total lines: $(wc -l < "$file")" # Contar las líneas en el archivo
+                echo "Total lines: $(wc -l < "$file")" # Contar las lineas en el archivo
             else
                 echo "File not found: $file" # Mensaje de error si el archivo no existe
             fi
@@ -52,12 +52,12 @@ while true; do
         5)
             read -p "Enter file path: " file
             read -p "Enter number of lines to display: " n
-            head -n "$n" "$file" # Mostrar las primeras n líneas del archivo
+            head -n "$n" "$file" # Mostrar las primeras n lineas del archivo
             ;;
         6)
             read -p "Enter file path: " file
             read -p "Enter number of lines to display: " n
-            tail -n "$n" "$file" # Mostrar las últimas n líneas del archivo
+            tail -n "$n" "$file" # Mostrar las últimas n lineas del archivo
             ;;
         7)
             echo "Exiting..."
